163642700e40cf0adb73bbd6be27a0defe08837c
mijimmi <jrmijares@up.edu.ph>
2024-05-22 15:27:29 +0800

    case three menu gamay

--- group_mode.cpp
+++ group_mode.cpp
@@ -40,7 +40,20 @@ void group_mode(){
 
 void case_one();
 void case_two();
-void case_three();
+void case_three(){
+    std::vector<Member> members;
+    int member_choice;
+    std:string member_name;
+
+    while (true){
+        std::cout << "\n Case 3 Menu:\n";
+        std::cout << "Group Budget Limiter\n";
+        std::cout << "1. Add Member.\n";
+        std::cout << "2. Display All Member Budgets.\n";
+        std::cout << "3. Add Expense for Member\n";
+        std::cout << "4. Exit\n";
+    }
+}
 
 void init(){
 
